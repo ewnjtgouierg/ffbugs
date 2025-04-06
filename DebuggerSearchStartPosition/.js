ea876a8f21b103f0276e6ecec4b7a9d4b0041a01
jaborandi <support@excsitement.com>
2025-04-06 21:12:18 +0300

    DebuggerSearchStartPosition

---
+++ DebuggerSearchStartPosition/.js
@@ -0,0 +1,151 @@
+debugger;
+
+class A
+	{
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+		run()
+			{
+				// code of class A run() method
+			}
+	}
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+class B
+	{
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+		run()
+			{
+				// code of class B run() method
+			}
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+// lots of code
+
+	}
+
